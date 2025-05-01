f272822165de0c6c4b5c9d62cc86c62e251844c5
Yonatan Sinai <shahar6771@gmail.com>
2025-05-01 20:06:25 +0300

    data adjustments

--- Config/data_tagging_config.py
+++ Config/data_tagging_config.py
@@ -15,8 +15,8 @@ LABELS_DECODER = {
 }
 
 # Prompt / for the Prompt
-PRO_ISRAEL_KEYWORDS = ['Hamas-ISIS (comparison between the two)', 'hostages', 'human shields', 'terrorist', 'antisemitism', 'Pallywood (insult for fake media comming from Gaza)']
-PRO_PALESTINE_KEYWORDS = ['IOF or diaper force (insult to the IDF)', 'Free Palestine', 'Genocide Joe (for Joe Biden)', 'zionist', 'isnotreal (instead of Israel)', 'war crime']
+PRO_ISRAEL_KEYWORDS = ['Hamas-ISIS (comparison between the two)', 'hostages (taken by Hamas as war tactic)', 'human shields (used by Hamas when they mask their operation behind civilians)', 'terrorists (in reference to the Islamic Jihad)', 'antisemitism (as a cause for the hatred and bias against Israel)', 'Pallywood (insult for fake media comming from Gaza)']
+PRO_PALESTINE_KEYWORDS = ['IOF or diaper force (insult to the IDF)', 'Free Palestine', 'Zionazis (or any comparison of zionists or Israel to Nazi Germany)', 'Genocide Joe (for Joe Biden)', 'apartheid (to the Palestinians by Israel / Settlers)', 'zionist (in a deminishing manner)', "Hasbara bot (or a general accusation of someone as being paid / operated by Israel's Hasbara office)", 'Isnotreal or Israhell (instead of Israel)', 'war crimes (by the IDF)']
 LABELING_INSTRUCTIONS = f"""
     You are a labeling assistant tasked with analyzing social-media comments regarding the Israel-Palestine conflict.
     The comments are written by independent users and may reflect political affiliations or perspectives related to one of the sides.
@@ -25,24 +25,43 @@ LABELING_INSTRUCTIONS = f"""
 
     "0" - The comment exhibits a clear affiliation to the Pro-Palestinian perspective.
     "1" - The comment exhibits a clear affiliation to the Pro-Israeli perspective.
-    "2" - The comment's affiliation cannot be confidently determined, or it is not relevant to this conflict.
-    To help you distinguish between perspectives:
+    "2" - The comment's affiliation cannot be confidently determined based on its content, or it is not relevant to this conflict. Also assign if the comment is pure sarcasm without stance context, a meme / gif, or only asks a question without clear stance.
+    
+    Here are a few guidelines To help you distinguish between perspectives:
 
-    Pro-Palestinian comments often focus on the perceived mistreatment of Palestinians, using terms such as "apartheid", "opression", "genocide," and criticizing settlements or the IDF, refering to them as if they commit those, or tying to compare them to terrorists. These comments may frame Palestinian military actions by Hamas as resistance against an "occupying force", mitigating the atrocities of October 7th claming it's propaganda, draw parallels to Hezbollah’s actions in the Lebanese front, and frequently critique the role of the U.S. and other Western governments in these conflicts, blaming jews for influencing them. Commonly used terms include: {', '.join(PRO_PALESTINE_KEYWORDS)}.
-    Pro-Israeli comments often emphasize the IDF's efforts to avoid civilian harm, refer to it as the most moral army, highlight Hamas's and similar groups disregard for Palestinian and Arab lives and prosperity, and describe actions by Hamas on October 7th, Hezbollah's and the Houthis since, as acts of terrorism or call for the release of the hostages. These comments may focus on the perceived Arab disregard for Western cultural values and liberal principles, LGBT as example case, positioning Israel as a defender of liberalism and democracy in the Middle East. Criticisms often extend to liberal figures or movements supporting the Palestinian side, citing conflicts of values, as well as the global influence of states like Iran and Russia. Commonly used terms include: {', '.join(PRO_ISRAEL_KEYWORDS)}. 
-    Both sides might try and claim they have ancestral ownership over the land. Jews / Israelis for thousands of years, while Palestinians will claim Jews came from europe.   
+    Pro-Palestinian comments:
+    - Often focus on the perceived mistreatment of Palestinians, using terms such as "apartheid", "opression", "genocide," and criticizing settlements or the IDF, refering to them as if they commit those, or tying to compare them to terrorists. 
+    - These comments may frame Palestinian military actions by Hamas as "resistance" against an "occupying force". 
+    - These comments will often try to mitigate the atrocities of October 7th state it is propaganda, justified, or will claim it's mostly self inflicted by the IDF to israely citizens.
+    - May express support for Hezbollah’s actions in the Lebanese front, or the Houtis in Yemen
+    - Might and frequently critique the role of the U.S. and other Western governments in these conflicts, blaming jews for influencing them. 
+    - Might claim the whole state of Israel is illegitimate, and / or to claim that jews and arabs lived peacefully before the inception of the state of Israel.
+    - Commonly used terms include: {', '.join(PRO_PALESTINE_KEYWORDS)} - Don't blindly classify based on these. Try to understand the underlying intention behind the usage of them.
+    
+    Pro-Israeli comments:
+    - Often emphasize the IDF's efforts to avoid civilian harm, may refer to it as the most moral army in the world, highlight this conflict's low combetant to civilians death ratio and Hamas's and other arab militant groups disregard for Palestinian and Arab lives and prosperity.
+    - Often describe actions by Hamas on October 7th, Hezbollah's and the Houthis since, as acts of terrorism.
+    - Will often call for the release of the hostages or will draw attention to the UN and Red Cross lousy involvement to ensure their safety.
+    - Will often criticize the international organizations (UN, UNRWA, ICJ) bias against the state of israel, and specifically in this conflict.
+    - These comments may focus on the perceived Arab disregard for Western cultural values and liberal principles, LGBT as example case, positioning Israel as a defender of liberalism and democracy in the Middle East. 
+    - Criticisms often extend to liberal figures or movements supporting the Palestinian side, citing conflicts of values, as well as the global influence of states like Iran and Russia.
+    - Might criticize comments about jews and arabs living peacefully before 1948, referencing violent events from the past of the explotation and expulsion of jews from arab states. 
+    - Commonly used terms include: {', '.join(PRO_ISRAEL_KEYWORDS)}. - Don't blindly classify based on these. Try to understand the underlying intention behind the usage of them.
+    
+    Both sides might try and claim they have ancestral ownership over the land. Jews / Israelis for thousands of years with historical ties to the land, while Palestinians will say their families lived in the region for generations claiming that the Jews came from europe and are colonial settlers.   
     
     Additional considerations:
      - Pay close attention to sarcasm or indirect expressions, which may subtly convey affiliation by mocking or discrediting the opposing perspective. Evaluate the tone and intent behind the comment to understand the commenter’s stance.
-     - Be mindful of quoted statements or references to prior comments. The context of the response (e.g., agreement, disagreement, or critique) should determine the affiliation, not the content of the quoted material.
+     - Try to associate the content of the comment with historical, cultural, political and current events, to understand the commenter’s political stance.
+     - Be mindful of quoted statements or references to prior comments, which are common in Reddit using "&gt;" mark. The context of the response (e.g., agreement, disagreement, or critique) should determine the affiliation, not the content of the quoted material / reference.
      - Identify criticism directed at narratives, claims, or terminology commonly used by the opposing side, as this often indicates the commenter’s alignment with their own perspective.
-     - Respond with only one of the labels (0, 1, or 2) based on the criteria above.
+     - Respond *only with one of the labels (0, 1, or 2)* based on the criteria above, and nothing else.
     """
     
 
 # LLM Configurations
 OPENAI_ENGINE = 'gpt-4o-mini'
-MAX_COMMENT_LENGTH = 500    # To avoid comments which are too long, limit length.
+MAX_COMMENT_LENGTH = 512    # To avoid comments which are too long for Bert's context window, limit length.
 BATCH_TOKENS_LIMIT = 1000000   # Based on OpenAI limitations which are 2,000,000. Account for calculation method and 1 additional comment after batch is too long.
 BATCH_REQUESTS_LIMIT = 50000    # Limit to the number of requests in a single batch
 TEMPERATURE = 0.0   # Level of randomness / creativity of the comment. Set to 0 to return the same response every time.
@@ -61,4 +80,4 @@ BATCH_ROW_FORMAT = {
 }
 
 TEST_BATCH_SIZE = 10    # Number of comments for a single test of the model
-TEST_MODE = False    # Will shrink the batch size, use the manually_tagged_data and calculate accuracy
+TEST_MODE = True    # Will shrink the batch size, use the manually_tagged_data and calculate accuracy
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Israel-Palestine-Political-Affiliation-Text-Classification
 
 ## Overview
-This project aims to develop a robust machine learning (ML) and deep learning (DL) framework for classifying social media comments related to the Israel-Palestine conflict into distinct political affiliations: Pro-Israel, Pro-Palestinian, and Undefined. A key component of this project is the **Automated Tagger**, which leverages a Large Language Model (LLM) to generate labeled datasets, addressing the challenge of untagged data in the original dataset.
+This project aims to develop a robust machine learning (ML) and deep learning (DL) framework for classifying social media comments related to the Israel-Palestine conflict into distinct political affiliations: Pro-Israel, Pro-Palestine and Undefined. A key component of this project is the **Automated Tagger**, which leverages a Large Language Model (LLM) to generate labeled datasets, addressing the challenge of untagged data in the original dataset.
 
 Using the automated tagger as benchmark and combining advanced contextual embeddings with optimized ML classifiers, this project seeks to set a new benchmark in nuanced text classification tasks with a simple, cheap and scaleable process.
 
@@ -25,7 +25,7 @@ Note the the current configurations are the ones for the best performing model b
 The `llm_tagger.py` module provides functionality to generate labels for an untagged dataset using the OpenAI API. Run the pipeline through `main.py`:
 
 - **Test Mode** (`TEST_MODE = True`):
-  - Tests the tagging process on a small, random subset of manually tagged data (`TAGGED_DATA_PATH`).
+  - Tests the tagging process on a small, random subset of manually tagged data (`TAGGED_DATA_PATH`) of `TEST_BATCH_SIZE` comments.
   - Outputs an Accuracy and F1 score and saves mismatched predictions for inspection in `OUTPUT_FILE_PATH`.
   - Ideal for fine-tuning the prompt and verifying the labeling quality.
 - **Batch Mode** (`TEST_MODE = False`):
@@ -33,10 +33,10 @@ The `llm_tagger.py` module provides functionality to generate labels for an unta
   - Outputs a fully labeled dataset in `OUTPUT_FILE_PATH`.
   - Used to prepare training data for ML/DL models.
 
-The automated tagger reached `F1 Score = 91%` on the manually tagged subset.
+The automated tagger reached `F1 Score = 91%` on the manually tagged subset (~1050 comments).
 
 ### Running the Dataset
-The `dataset.py` module is pretty straight forward and can be run through `main.py`. Adjust the path and choose the subset you wish to work with (pre-divided), set up the file paths for the research data and vectorization weights, set augmentation ratio, traget groups and the data shape of the dataloader, meaning - this module will create a textual dataloader on defualt, assuming you have yet trained a model, but on demand will create a vectorized dataset (with embeddings for comments), under the assumption you have already fine-tuned a model, and have it's weights available. The vectorized dataset, once created under certain configurations will be saved as a pickle file to act as cache. If you'll delete this file you'll have to re-calculate the dataset. Not recommended.
+The `dataset.py` module is pretty straight forward and can be run through its `__main__`. Adjust the path and choose the subset you wish to work with (pre-divided), set up the file paths for the research data and vectorization weights, set augmentation ratio, traget groups and the data shape of the dataloader, meaning - this module will create a textual dataloader on defualt, assuming you have yet trained a model, but on demand will create a vectorized dataset (with embeddings for comments), under the assumption you have already fine-tuned a model, and have it's weights available. The vectorized dataset, once created under certain configurations will be saved as a pickle file to act as cache. If you'll delete this file you'll have to re-calculate the dataset. Not recommended.
 This module uses the `embedder.py` module for that, and will create a dataloder fit for the classification task. Before calling a classification task be sure to set in `dataset_config.py` the parameter `DATALOADER_SHAPE = 'embedding'`, and pay attention that the `EMBEDDING_METHOD` matches your intention.
 
 The trained weights for both models can be loaded from [this link](https://drive.google.com/drive/folders/1gNbb4B03qY2LVFy61dkgW_Ryf18YuRSm?usp=sharing). The Best DistilBERT model was trained using the configurations described in the `Analysis\fit_embeddings.ipynb` and reached val_loss=0.2176 on the 3rd epoch. Both TFIDF model and DistilBERT pretrain folders are there. Place under local 'Embedding' folder to match with the paths in `dataset_config.py`
@@ -46,7 +46,7 @@ The `classifiers.py` module controls the training pipeline. It assumes you are a
 
 When activated directly, this module allows you to test different configurations manually and assess the models while training on `SUBSET=TRAIN` and testing on `SUBSET=TEST`.
 
-The classifier reached a total F1 score of `83.6%` on the complete test set.
+The classifier reached a total F1 score of `83.6%` on the complete test set (~43K comments).
 
 ### Installation
 Set up the environment and install dependencies:
@@ -58,7 +58,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 ```
 
-## Git Updates
+## Git Commit Tips
 ```
 git add .
 git commit -m "commit message"
@@ -70,6 +70,6 @@ In this link, you may find the final draft for this paper, hoping it will be pub
 [📄 View Final Draft](https://drive.google.com/file/d/1NNCZqMsk0gYNtxtj4JviBk_mlpi88th0/view?usp=sharing)
 
 # Related Work: Israel-Palestine-War-Reddit-Analysis
-For users interested in analyzing the dynamics of this social media discussion, I recommend my related project: [Israel-Palestine Text Analysis](https://github.com/shaharoded/Israel-Palestine-War-Reddit-Analysis). This project provides a visualization dashboard to explore and compare Pro-Israel and Pro-Palestinian online behavior based on various NLP metrics, including sentiment distribution, toxicity, and topic-based analysis. Built using Streamlit and Plotly, it enables interactive, within- and between-group comparisons of speech styles and tendencies.
+For users interested in analyzing the dynamics of this social media discussion, I recommend my related project: [Israel-Palestine Text Analysis](https://github.com/shaharoded/Israel-Palestine-War-Reddit-Analysis). This project provides a visualization dashboard to explore and compare Pro-Israelis and Pro-Palestinians online behavior based on various NLP metrics, including sentiment distribution, toxicity, and topic-based analysis. Built using Streamlit and Plotly, it enables interactive, within- and between-group comparisons of speech styles and tendencies.
 
 The predictions generated by the classification pipeline from this project are utilized in the analysis project to enhance its plots and offer updated insights into political discourse trends. Together, these projects form a robust framework for studying polarized conversations in conflict-driven contexts.
